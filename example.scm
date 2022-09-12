f07e3ca55e9130b97616091d025ffc1b1ea06961
jgod <3506872+jgod@users.noreply.github.com>
2022-09-12 04:35:13 -0400

    formatting

--- example.scm
+++ example.scm
@@ -42,37 +42,32 @@
 
 (define (string-upper str) 
     (list->string 
-        (map 
-            (lambda (c) (char-upcase c))
-            (string->list (string-copy str)))))
+        (map (lambda (c) (char-upcase c))
+             (string->list (string-copy str)))))
 
 (define (string-lower str) 
     (list->string 
-        (map 
-            (lambda (c) (char-downcase c))
-            (string->list (string-copy str)))))
+        (map (lambda (c) (char-downcase c))
+             (string->list (string-copy str)))))
 
 (define (string-trim-left str) 
     (let ((found-nonwhite #f))
         (list->string 
-            (filter 
-                (lambda (c) 
-                    (if (equal? found-nonwhite #t)
-                        c
-                        (if (not (equal? c #\space)) 
-                            (begin (set! found-nonwhite #t) c) 
-                            (not (equal? c #\space)))))
-                (string->list (string-copy str))))))
+            (filter (lambda (c) 
+                        (if (equal? found-nonwhite #t)
+                            c
+                            (if (not (equal? c #\space)) 
+                                (begin (set! found-nonwhite #t) c) 
+                                (not (equal? c #\space)))))
+                    (string->list (string-copy str))))))
 
 (define (string-trim-right str) (string-reverse (string-trim-left (string-reverse str))))
 
 (define (string-trim str) (string-trim-right (string-trim-left str)))
 
 (define (string-compact str) 
-    (list->string
-        (filter 
-            (lambda (c) (not (equal? c #\space)))
-            (string->list (string-copy str)))))
+    (list->string (filter (lambda (c) (not (equal? c #\space)))
+                          (string->list (string-copy str)))))
 
 (define (string-truncate str maxlen replacement)
     (if (> (string-length str) maxlen)
