6b5a4557fe74717abce06913aec2a3d2806bbb31
jgod <3506872+jgod@users.noreply.github.com>
2022-09-12 04:18:23 -0400

    .

--- example.scm
+++ example.scm
@@ -8,6 +8,10 @@
                 (- n 1) 
                 (cdr lst)))))
 
+; push/pop
+(define* (push lst . r) (append lst r '()))
+(define (pop lst) (reverse (cdr (reverse lst))))
+
 ; head/tail
 (define (head lst) (list-ref lst 0))
 (define (tail lst) (reverse (pop (reverse lst))))
@@ -16,15 +20,10 @@
 (define (first lst) (list-ref lst 0))
 (define (last lst) (list-ref (reverse lst) 0))
 
-; push/pop
-(define* (push lst . r) (append lst r '()))
-(define (pop lst) (reverse (cdr (reverse lst))))
-
 ; remove is the opposite of filter.
 (define (simple-remove lst el) (filter (lambda (x) (not (equal? x el))) lst))
 ; a better version, remove any number of things from a list.
 (define* (remove lst . r) (filter (lambda (x) (not (member x r))) lst))
-
 
 ; length for either string or list
 (define (len x) (if (string? x) (string-length x) (length x)))
@@ -45,13 +44,13 @@
     (list->string 
         (map 
             (lambda (c) (char-upcase c))
-            (string->list (string->copy str)))))
+            (string->list (string-copy str)))))
 
 (define (string-lower str) 
     (list->string 
         (map 
             (lambda (c) (char-downcase c))
-            (string->list (string->copy str)))))
+            (string->list (string-copy str)))))
 
 (define (string-trim-left str) 
     (let ((found-nonwhite #f))
@@ -65,7 +64,7 @@
                             (not (equal? c #\space)))))
                 (string->list (string-copy str))))))
 
-(define (string-trim-left str) (reverse (string-trim-left (reverse str))))
+(define (string-trim-right str) (string-reverse (string-trim-left (string-reverse str))))
 
 (define (string-trim str) (string-trim-right (string-trim-left str)))
 
@@ -76,6 +75,8 @@
             (string->list (string-copy str)))))
 
 (define (string-truncate str maxlen replacement)
-    (string-append 
-        (list->string (take maxlen (string->list (string-copy str)))) 
-        replacement))
+    (if (> (string-length str) maxlen)
+        (string-append 
+            (list->string (take maxlen (string->list (string-copy str)))) 
+            replacement)
+        str))
